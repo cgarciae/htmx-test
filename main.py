a2e29efc2edf6ff9b18fe09db7f73269a7b4653b
Cristian Garcia <cgarcia.e88@gmail.com>
2023-09-01 20:25:34 -0500

    todo

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from pathlib import Path
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Form, Request
 from fastapi.responses import HTMLResponse
+from typing import Annotated, Any
 
 # include jinja
 from fastapi.templating import Jinja2Templates
@@ -16,42 +17,56 @@ if not DB_PATH.exists():
     json.dump({"count": 0}, DB_PATH.open("w"))
 
 
-async def load_db():
+async def load_db() -> dict[str, Any]:
     async with aiofiles.open(DB_PATH, mode="r") as f:
         return json.loads(await f.read())
 
 
-async def save_db(db):
+async def save_db(db: dict[str, Any]):
     async with aiofiles.open(DB_PATH, mode="w") as f:
         await f.write(json.dumps(db))
 
 
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
-    # read the templates/index.html.jinja files
+    # read the templates/index.jinja files
     db = await load_db()
     return templates.TemplateResponse(
-        "index.html.jinja", {"request": request, "count": db["count"]}
+        "index.jinja", dict(request=request, todos=db["todos"])
     )
 
 
-@app.post("/inc", response_class=HTMLResponse)
-async def inc(request: Request):
+@app.post("/todos/{id}", response_class=HTMLResponse)
+async def save(
+    request: Request,
+    id: str,
+    title: Annotated[str, Form()] = "",
+    text: Annotated[str, Form()] = "",
+):
+    db = await load_db()
+    todo = db["todos"][id]
+    todo.update({"title": title, "text": text})
+    await save_db(db)
+
+    return templates.TemplateResponse("todo.jinja", dict(request=request, todo=todo))
+
+
+# new todo
+@app.put("/todos", response_class=HTMLResponse)
+async def new(request: Request):
     db = await load_db()
     db["count"] += 1
+    id = db["count"]
+    todo = {"id": id, "title": "", "text": ""}
+    db["todos"][f"{id}"] = todo
     await save_db(db)
 
-    return templates.TemplateResponse(
-        "counter.html.jinja", {"request": request, "count": db["count"]}
-    )
+    return templates.TemplateResponse("todo.jinja", dict(request=request, todo=todo))
 
 
-@app.post("/dec", response_class=HTMLResponse)
-async def dec(request: Request):
+# delete todo
+@app.delete("/todos/{id}")
+async def delete(id: str):
     db = await load_db()
-    db["count"] -= 1
+    del db["todos"][id]
     await save_db(db)
-
-    return templates.TemplateResponse(
-        "counter.html.jinja", {"request": request, "count": db["count"]}
-    )
